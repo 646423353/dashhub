62dcb911214fc5365318c7d4d3f9104e111470dd
yangkai <646423353@qq.com>
2026-03-22 03:14:17 +0800

    chore: 更新生产环境配置
    - 添加生产域名到CORS白名单
    - 更新FRONTEND_URL为生产域名

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,7 +26,9 @@ const allowedOrigins = [
   'http://localhost:5174',
   'http://localhost:5175',
   'http://localhost:5176',
-  'http://localhost:3000'
+  'http://localhost:3000',
+  'https://dashhub.insfair.cn',
+  'https://cehua.insfair.cn',
 ];
 
 app.use(cors({
